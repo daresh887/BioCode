3ec4a93ef493f397bc93a9c836dfb673d8db7359
HarryPotter12352 <79324017+HarryPotter12352@users.noreply.github.com>
2021-10-23 19:58:01 +0530

    0.2.0 | Express setup

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,17 @@
-const stringToSend = "Hello World!";
-console.log(stringToSend);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
+const app = (0, express_1.default)();
+const PORT = 2000;
+app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
+app.get("/", (req, res) => {
+    res.sendStatus(200);
+});
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`);
+});
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-const stringToSend = "Hello World!";
-console.log(stringToSend);
+import express from "express";
+import path from "path";
+
+const app = express();
+const PORT = 2000;
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.get("/", (req, res) => {
+  res.sendStatus(200); 
+})
+
+
+app.listen(PORT, () => {
+  console.log(`App is listening at http://localhost:${PORT}`);
+})
